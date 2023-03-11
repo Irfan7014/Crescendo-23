95106f0df23b4545a934172a4b5aa662aa8f63c7
Liny Mathew <8950.liny.secompb@gmail.com>
2023-03-11 16:22:37 +0530

    login

--- Frontend/src/app/Registrations/login/login.component.ts
+++ Frontend/src/app/Registrations/login/login.component.ts
@@ -7,4 +7,31 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  email: string = ''
+  password:string = ''
+  hidePassword:boolean = true;
+  isValidPassword:boolean = false;
+  checkLoginDetails(){
+
+  }
+
+  validatePasswordAndOTP(value:string, type:string) {
+    var decimal = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{7,}$/;
+    if (String(value).match(decimal)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  onPasswordValidation() {
+    if (this.password.length > 8 && this.validatePasswordAndOTP(this.password, 'password'))
+      this.isValidPassword = true;
+    else 
+      this.isValidPassword = false;
+  }
+
+  login(){
+    
+  }
 }
