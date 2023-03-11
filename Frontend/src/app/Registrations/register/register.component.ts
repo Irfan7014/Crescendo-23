b0e1cb67f7b5f580f50ad2bf9d0bb9213ae5458f
Irfan Abidi <57263241+Irfan7014@users.noreply.github.com>
2023-03-11 15:58:28 +0530

    Register Component

--- Frontend/src/app/Registrations/register/register.component.ts
+++ Frontend/src/app/Registrations/register/register.component.ts
@@ -6,5 +6,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+  fullName: string = ''
+  email: string = ''
+  password:string = ''
+  hidePassword:boolean = true;
+  isValidPassword:boolean = false;
+  checkRegisterDetails(){
+
+  }
+  validateEmail(email: string) {
+    var validRegex =
+      /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    if (email.match(validRegex)) {
+      return true;
+    }
+    return false;
+  }
+
+  validatePasswordAndOTP(value:string, type:string) {
+    var decimal = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{7,}$/;
+    if (String(value).match(decimal)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  onPasswordValidation() {
+    if (this.password.length > 8 && this.validatePasswordAndOTP(this.password, 'password'))
+      this.isValidPassword = true;
+    else 
+      this.isValidPassword = false;
+  }
+
+  register(){
+    
+  }
 
 }
