0d122d38ce3d7ad173dcf5ec162f96f8d0dbcb07
Irfan Abidi <57263241+Irfan7014@users.noreply.github.com>
2023-03-11 14:02:14 +0530

    Registrations page - Components Created

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './Registrations/register/register.component';
+import { LoginComponent } from './Registrations/login/login.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    LoginComponent,
+    LandingPageComponent
   ],
   imports: [
     BrowserModule,
